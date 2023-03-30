eb8f41cde15fe3b5f4f215dc2bfe95a6ce71305b
rhazek <matheus_rhazek@hotmail.com>
2023-03-30 12:09:12 -0300

    preenchendo as últimas funções

--- service.js
+++ service.js
@@ -1,15 +1,18 @@
 const validarEntradaDeDados = (lancamento) => {
-   if(!lancamento.cpf) return 'CPF não informado, tente novamente'
-   if(!lancamento.valor) return 'Valor não informado, tente novamente'
-   if(lancamento.valor < -2000.00) return 'Valor não pode ser inferior a -2000,00, digite um valor válido'
-   if(lancamento.valor > 15000.00) return 'Valor não pode ser superior a 15000,00, digite um valor válido'
-   if(lancamento.cpf.length != 11) return 'CPF deve conter 11 dígitos'
-   if(!lancamento.cpf.match(/^[0-9]+$/)) return 'CPF deve conter apenas números'
-   return null
-}
+  if (!lancamento.cpf) return "CPF não informado, tente novamente";
+  if (!lancamento.valor) return "Valor não informado, tente novamente";
+  if (lancamento.valor < -2000.0)
+    return "Valor não pode ser inferior a -2000,00, digite um valor válido";
+  if (lancamento.valor > 15000.0)
+    return "Valor não pode ser superior a 15000,00, digite um valor válido";
+  if (lancamento.cpf.length != 11) return "CPF deve conter 11 dígitos";
+  if (!lancamento.cpf.match(/^[0-9]+$/))
+    return "CPF deve conter apenas números";
+  return null;
+};
 
 const recuperarSaldosPorConta = (lancamentos) => {
-   const saldos = {};
+  const saldos = {};
   for (const lancamento of lancamentos) {
     if (lancamento.cpf && lancamento.valor) {
       if (!saldos[lancamento.cpf]) {
@@ -20,7 +23,7 @@ const recuperarSaldosPorConta = (lancamentos) => {
     }
   }
   return Object.entries(saldos).map(([cpf, valor]) => ({ cpf, valor }));
-}
+};
 
 const recuperarMaiorMenorLancamentos = (cpf, lancamentos) => {
   const lancamentosCpf = lancamentos.filter(
@@ -43,27 +46,46 @@ const recuperarMaiorMenorLancamentos = (cpf, lancamentos) => {
   }
 };
 
-
 const recuperarMaioresSaldos = (lancamentos) => {
-  const saldos = {}; // objeto para armazenar os saldos dos CPFs
-
-  // calcula o saldo para cada CPF
-  lancamentos.forEach((lancamento) => {
-    if (!saldos[lancamento.cpf]) {
-      saldos[lancamento.cpf] = 0; // inicializa o saldo com 0 se o CPF ainda não estiver no objeto
+  const saldos = {};
+  for (const lancamento of lancamentos) {
+    if (lancamento.cpf && lancamento.valor) {
+      if (!saldos[lancamento.cpf]) {
+        saldos[lancamento.cpf] = lancamento.valor;
+      } else {
+        saldos[lancamento.cpf] += lancamento.valor;
+      }
     }
-    saldos[lancamento.cpf] += lancamento.valor; // adiciona o valor do lançamento ao saldo correspondente
-  });
-
-  const topCPFs = Object.keys(saldos) // obtém as chaves do objeto (os CPFs)
-    .map((cpf) => ({ cpf, saldo: saldos[cpf] })) // mapeia cada CPF para um objeto com o CPF e o saldo
-    .sort((a, b) => b.saldo - a.saldo) // ordena os objetos com base no saldo (do maior para o menor)
-    .slice(0, 3); // pega apenas os 3 primeiros objetos (os CPFs com maiores saldos)
-
-  return topCPFs.length > 0 ? topCPFs : []; // retorna o array com os 3 CPFs com maiores saldos ou um array vazio se não houver registros correspondentes
-
-}
+  }
+  const topCPFs = Object.keys(saldos)
+    .map((cpf) => ({ cpf, valor: saldos[cpf] }))
+    .sort((a, b) => b.valor - a.valor)
+    .slice(0, 3);
+  return topCPFs;
+};
 
 const recuperarMaioresMedias = (lancamentos) => {
-    return []
-}
+  const saldos = {};
+
+  for (const lancamento of lancamentos) {
+    const cpf = lancamento.cpf;
+    const valor = lancamento.valor;
+
+    if (!saldos[cpf]) {
+      saldos[cpf] = {
+        saldo: 0,
+        contador: 0,
+      };
+    }
+
+    saldos[cpf].saldo += valor;
+    saldos[cpf].contador++;
+  }
+
+  const saldosMedios = Object.keys(saldos).map((cpf) => ({
+    cpf,
+    valor: saldos[cpf].saldo / saldos[cpf].contador,
+  }));
+  saldosMedios.sort((a, b) => b.valor - a.valor);
+  return saldosMedios.slice(0, 3);
+};
